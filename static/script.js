d89ffc08ac7929d47861b9145fb58cb3c107b378
Katrina Chan <katrina.hyc@gmail.com>
2017-06-05 04:35:22 +0800

    Fix syntax error

--- static/script.js
+++ static/script.js
@@ -5,17 +5,23 @@ var $$ = function (s) {
   return document.querySelectorAll(s);
 }
 
-$$('.date').forEach(function (it) {
-  it.addEventListener('click', function (ev) {
+var dates = $$('.date');
+
+for (var i = 0; i < dates.length; ++i) {
+  dates[i].addEventListener('click', function (ev) {
     ev.preventDefault()
     $('#chosen').value = ev.target.innerHTML;
-    $$('.date').forEach(function (itt) {itt.className = 'date'; });
-    var idx = ev.target.id.split('-')[2]
-    $('#article-date-' + idx).className += ' chosen'
-    $('#list-date-' + idx).className += ' chosen'
-  })
-})
+    for (var j = 0; j < dates.length; ++j) {
+      dates[j].className = 'date';
+    }
+    var idx = ev.target.id.split('-')[2];
+    $('#article-date-' + idx).className += ' chosen';
+    $('#list-date-' + idx).className += ' chosen';
+  });
+}
 
 $('#chosen').addEventListener('input', function (ev) {
-  $$('.date').forEach(function (itt) {itt.className = 'date'; });
+  for (var i = 0; i < dates.length; ++i) {
+    dates[i].className = 'date';
+  }
 })
