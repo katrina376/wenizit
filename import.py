a49ed7996c7c5196b2866a6e1f46e49d69ef5971
Katrina Chan <katrina.hyc@gmail.com>
2017-06-07 17:56:26 +0800

    Update

--- import.py
+++ import.py
@@ -1,16 +1,14 @@
 from pymongo import MongoClient
 import os
 import json
-
+'''
 MONGODB_URI = os.environ['MONGODB_URI']
 client = MongoClient(MONGODB_URI)
 db = client.wenizit
 db.judgements.drop()
 judgements = db.judgements
-
+'''
 with open('data/data.json', 'r') as f:
     string = f.read()
     judgement = json.loads(string)
-    for j in judgement:
-        j['dates'] = []
-        judgements.insert_one(j)
+    print(judgement[7485])
